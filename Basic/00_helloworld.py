6a9fd170c4884d493d61c67c6ebc6c383a5bad19
EDGE <edisonbrito1@gmail.com>
2025-12-09 00:11:01 -0500

    build: :bricks: first_commit
    Initial commit create folders and docs

---
+++ Basic/00_helloworld.py
@@ -0,0 +1,13 @@
+#Hello World
+print("Hello, World!")
+
+
+"""
+Esto es un comentario
+de varias lineas
+"""
+
+# Otra forma de imprimir
+mensaje = "Hola, Mundo!"
+print(mensaje)  
+# Fin del programa
---
+++ Basic/01_variables.py
@@ -0,0 +1,81 @@
+#VARIABLES
+
+"Cadena de texto"
+"String str"
+my_str = "Hello, I'm a string"
+print(my_str)
+print(type(my_str))
+print(len(my_str))
+print(my_str[0])
+print(my_str[-1])
+print(my_str[0:5])
+print(my_str.upper())
+
+"Numeros Enteros"
+"Integer int"
+my_int = 10
+print(my_int)
+print(type(my_int))
+print(my_int + 5)
+print(my_int * 2)
+print(my_int / 3)
+print(my_int // 3)
+
+"Numeros Decimales"
+"Float float"
+my_float = 10.5
+print(my_float)
+print(type(my_float))
+print(my_float + 2.5)
+print(my_float * 2)
+print(my_float / 2)
+print(my_float // 2)
+
+"Booleanos"
+"Boolean bool"
+my_bool = True
+print(my_bool)
+print(type(my_bool))
+print(not my_bool)
+print(my_bool and False)
+print(my_bool or False)
+print(my_bool == False)
+print(my_bool != False)
+print(int(my_bool))
+print(float(my_bool))
+print(str(my_bool))
+
+"Listas"
+"List list"
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+print(type(my_list))
+print(my_list[0])
+print(my_list[-1])
+print(my_list[1:4])
+my_list.append(6)
+print(my_list)
+my_list.remove(3)
+print(my_list)
+print(len(my_list))
+print(sum(my_list))
+print(max(my_list))
+print(min(my_list))
+my_list.sort()
+print(my_list)
+
+"Tuplas"
+"Tuple tuple"
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:4])
+print(len(my_tuple))
+print(sum(my_tuple))
+print(max(my_tuple))
+print(min(my_tuple))
+
+
+
